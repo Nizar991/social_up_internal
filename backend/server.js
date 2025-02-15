a62b5dda76a00ba411cf179d8881934edfd8a9e1
Nizar Ahmed <115566324+Nizar991@users.noreply.github.com>
2025-02-15 09:12:25 +0600

    Updated authRoutes and server configurations

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const authRoutes = require('./authRoutes');
+const authRoutes = require('./routes/authRoutes');
 const Line = require('./models/Line');  // Import the Line model here
 
 dotenv.config();
